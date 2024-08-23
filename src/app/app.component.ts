95b597e91a816a056bebf2086d878bf92802664b
Brandon Carlos <brandon_marzo@hotmail.com>
2024-08-23 13:38:53 -0300

    Adding components, reusable classes with our own styles, fortawesome and declaring the Category type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from "primeng/button";
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './share/font-awesome-icons';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ButtonModule, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'aibnb-clone-front';
+export class AppComponent implements OnInit{
+
+  faIconLibrary = inject(FaIconLibrary)
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
---
+++ src/app/share/font-awesome-icons.ts
@@ -0,0 +1,6 @@
+import { faAirbnb } from "@fortawesome/free-brands-svg-icons";
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+
+export const fontAwesomeIcons: IconDefinition[] = [
+    faAirbnb //There are lot of icons, then for my application started will small bundle I go will add icon per icon over here
+]
